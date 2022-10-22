17a673f84789dfd5dd7839283876e6732fe89192
Sohyeon <needtlc13@gmail.com>
2022-10-22 22:35:01 +0900

    feat: implement run integrated code and show the result

--- src/App.js
+++ src/App.js
@@ -17,12 +17,23 @@ import ToolBar from "./components/ToolBar";
 export default function App() {
   const [selectedLanguage, setSelectedLanguage] = useState("html");
   const [code, setCode] = useState({
-    html: { content: "html", prev: null, next: null },
+    html: { content: "<h1>This is h1 element</h1>", prev: null, next: null },
     css: { content: "h1 {\n color: red;\n}", prev: null, next: null },
-    js: { content: "js", prev: null, next: null },
+    js: {
+      content:
+        'document.querySelector("h1").addEventListener("click", function () {\n  alert("clicked h1 element");\n});',
+      prev: null,
+      next: null,
+    },
   });
+  const [isRunCliked, setIsRunCliked] = useState(false);
 
   const selectedLanguageCode = code[selectedLanguage].content;
+
+  const handleLanguageClick = (language) => {
+    setIsRunCliked(false);
+    setSelectedLanguage(language);
+  };
 
   return (
     <Layout>
@@ -34,17 +45,20 @@ export default function App() {
           code={code}
           handleClick={setCode}
           selectedLanguage={selectedLanguage}
+          handleRunClick={() => setIsRunCliked(true)}
         />
       </AppHeader>
       <ContentBox>
         <LanguageBar
           selectedLanguage={selectedLanguage}
-          handlePress={(language) => setSelectedLanguage(language)}
+          handlePress={handleLanguageClick}
         />
         <CodeArea
           code={selectedLanguageCode}
+          wholeCode={code}
           handleChange={setCode}
           selectedLanguage={selectedLanguage}
+          isRunCliked={isRunCliked}
         />
         <ToolBar
           handleChange={(str) =>
--- src/components/CodeArea.js
+++ src/components/CodeArea.js
@@ -1,12 +1,18 @@
+import { useState, useCallback } from "react";
+
 import PropTypes from "prop-types";
+import styled from "styled-components";
 
 import CodeMirror from "@uiw/react-codemirror";
 import { javascript } from "@codemirror/lang-javascript";
 import { html } from "@codemirror/lang-html";
 import { css } from "@codemirror/lang-css";
 import { atomone } from "@uiw/codemirror-theme-atomone";
-import { useCallback, useState } from "react";
+
 import debounce from "../utils/debounce";
+import integrateCode from "../utils/integrateCode";
+
+import { DARK_BLUE_100 } from "../constants/color";
 
 function selectLanguageExtension(string) {
   if (string === "html") return [html()];
@@ -14,8 +20,19 @@ function selectLanguageExtension(string) {
   if (string === "js") return [javascript({ jsx: true })];
 }
 
-export default function CodeArea({ code, handleChange, selectedLanguage }) {
+export default function CodeArea({
+  code,
+  wholeCode,
+  handleChange,
+  selectedLanguage,
+  isRunCliked,
+}) {
   const [currentCode, setCurrentCode] = useState(code);
+  const {
+    html: { content: htmlCode },
+    css: { content: cssCode },
+    js: { content: jsCode },
+  } = wholeCode;
 
   const saveContent = (text, prevText, language) => {
     if (text === prevText) return;
@@ -52,19 +69,48 @@ export default function CodeArea({ code, handleChange, selectedLanguage }) {
 
   const saveContentDebounce = useCallback(debounce(saveContent), []);
   return (
-    <CodeMirror
-      value={code}
-      theme={atomone}
-      extensions={selectLanguageExtension(selectedLanguage)}
-      onBlur={handleBlur}
-      onChange={handleCodeMirrorChange}
-      onKeyUp={handleKeyUp}
-    />
+    <Container>
+      {isRunCliked ? (
+        <ResultViewer srcDoc={integrateCode(htmlCode, cssCode, jsCode)} />
+      ) : (
+        <CodeMirror
+          value={code}
+          theme={atomone}
+          extensions={selectLanguageExtension(selectedLanguage)}
+          onBlur={handleBlur}
+          onChange={handleCodeMirrorChange}
+          onKeyUp={handleKeyUp}
+          className="editor"
+        />
+      )}
+    </Container>
   );
 }
 
 CodeArea.propTypes = {
   code: PropTypes.string,
+  wholeCode: PropTypes.object.isRequired,
   handleChange: PropTypes.func,
   selectedLanguage: PropTypes.string.isRequired,
+  isRunCliked: PropTypes.bool.isRequired,
 };
+
+const Container = styled.div`
+  display: flex;
+  flex: 14;
+  width: 100%;
+  height: 100%;
+
+  .editor {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const ResultViewer = styled.iframe`
+  width: 100%;
+  height: 100%;
+  background: ${DARK_BLUE_100};
+  border: none;
+  outline: none;
+`;
