101279542fee91a1abd7606da8c7917081ba22ab
taeuk <dcjimin@gmail.com>
2022-10-28 20:55:21 +0900

    refactor: change isloaded

--- src/App.js
+++ src/App.js
@@ -188,7 +188,7 @@ export default function App() {
 
   return (
     <>
-      {!isLoaded && (
+      {isLoaded && (
         <Layout innerHeight={innerHeight}>
           <AppHeader>
             <MenuWrapper>
